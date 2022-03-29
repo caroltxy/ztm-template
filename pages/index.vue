4e328d384cb63a331d3b76a615d870b07ef7dcec
caroltxy <caroltxy@hotmail.com>
2022-03-30 00:42:56 +0800

    updated homepage with new title and slogan

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+
+    <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
-    </div>
-  </div>
+      <h1 class="text-5xl"><b>Welcome to My First Web Page!</b></h1>
+      <p class="mt-5" style="color:pink; font: size 4.2em;">  <b>Dream Come True!</b></p>
+      <p>&#128512;</p>
+      <iframe width="750" height="400" src="https://www.youtube.com/embed/4RMOI7rLo5M"/>
+      <img src="https://c.tadst.com/gfx/750w/make-your-dreams-come-true-day-fun.jpg" width="750" height="700">
+      </div>
+      </div>
+        </div>
 </template>
