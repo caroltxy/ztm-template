bc412928ef6f258ba23fa33611c0817386a4fd4b
caroltxy <caroltxy@hotmail.com>
2022-03-30 14:42:02 +0800

    revised version

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
       <h1 class="text-5xl"><b>Welcome to My First Web Page!</b></h1>
-      <p class="mt-5" style="color:pink; font: size 4.2em;">  <b>Dream Come True!</b></p>
+      <p class="mt-5" style="color:pink; font: size 10.2em;">  <b>Dream Come True!</b></p>
       <p>&#128512;</p>
       <iframe width="750" height="400" src="https://www.youtube.com/embed/4RMOI7rLo5M"/>
       <img src="https://c.tadst.com/gfx/750w/make-your-dreams-come-true-day-fun.jpg" width="750" height="700">
